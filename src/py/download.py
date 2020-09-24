ce27c56653588632a8e15fd2264fd75b1ce68271
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-09-24 07:17:26 -0400

    Add code for download

---
+++ src/py/download.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+import requests
+
+# Make sure target directory exists
+data = Path("data")
+data.mkdir(parents=True, exist_ok=True)
+
+# Make request
+url = "FILLERFROMAMAR"
+r = requests.get(url)
+
+# Download
+print("Downloading reference. This may take awhile...")
+with open(data / "reference.rds", "wb") as file:
+    file.write(r.content)
