528f4e6b309af4df0cd9071d2852df74426ffb64
rbpatt2019 <ryan.patterson.2015@gmail.com>
2020-10-27 06:36:25 -0400

    Add additional query message

--- src/py/download.py
+++ src/py/download.py
@@ -8,6 +8,7 @@ data = Path("data")
 data.mkdir(parents=True, exist_ok=True)
 
 # Make request
+print("Requesting reference from website. This may take awhile...")
 url = "https://seqseek.ninds.nih.gov/data/classify/reference.rds"
 r = requests.get(url)
 
